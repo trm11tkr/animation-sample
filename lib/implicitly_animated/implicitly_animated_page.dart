6f38aa518e773927e81bdf990aa4f7766a39561c
森田陸斗 <mrmrt1121@gmail.com>
2022-08-27 13:41:43 +0900

    AnimatedOpacityPageを追加

--- lib/implicitly_animated/implicitly_animated_page.dart
+++ lib/implicitly_animated/implicitly_animated_page.dart
@@ -2,6 +2,7 @@ import 'package:animations_sample/implicitly_animated/animated_align.dart';
 import 'package:animations_sample/implicitly_animated/animated_container.dart';
 import 'package:animations_sample/implicitly_animated/animated_cross_fade.dart';
 import 'package:animations_sample/implicitly_animated/animated_default_text_style.dart';
+import 'package:animations_sample/implicitly_animated/animated_opacity.dart';
 import 'package:animations_sample/widgets/app_list_tile.dart';
 import 'package:animations_sample/widgets/app_scaffold.dart';
 import 'package:flutter/material.dart';
@@ -32,6 +33,10 @@ class ImplicitlyAnimatedPage extends StatelessWidget {
           AppListTile(
             title: 'animatedDefaultTextStyle',
             nextRouteName: AnimatedDefaultTextStylePage.routeName,
+          ),
+          AppListTile(
+            title: 'animatedOpacityPage',
+            nextRouteName: AnimatedOpacityPage.routeName,
           ),
         ],
       ),
