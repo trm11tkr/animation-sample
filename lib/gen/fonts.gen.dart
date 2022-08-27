700643497f55bfe425a4776fb5ec2f760229e326
森田陸斗 <mrmrt1121@gmail.com>
2022-08-27 13:10:50 +0900

    新しいフォントを導入

---
+++ lib/gen/fonts.gen.dart
@@ -0,0 +1,18 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import
+
+class FontFamily {
+  FontFamily._();
+
+  /// Font family: AlumniSansPinstripe
+  static const String alumniSansPinstripe = 'AlumniSansPinstripe';
+
+  /// Font family: MPLUSRounded1c
+  static const String mPLUSRounded1c = 'MPLUSRounded1c';
+}
